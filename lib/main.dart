86a471021356e515d25687eff0cd0f2a6b070fed
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-25 11:58:50 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyWidget extends StatefulWidget {
 
 class _MyWidgetState extends State<MyWidget> {
   int compteur = 0;
+double degre = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +36,18 @@ Icon(Icons.refresh)),
  SizedBox(width: 10),
  IconButton(onPressed: () { setState(() { compteur++; }); }, icon:
 Icon(Icons.add)),
+Text("Merci de choisir le degré de rotation"),
+Slider(
+ value: degre,
+ min: 0,
+ max: 360,
+ onChanged: (value) {
+ setState(() {
+ degre = value;
+ });
+ },
+),
+Text("Le degré choisi est${degre.toStringAsFixed(0)}")
  ],
 ),
 
